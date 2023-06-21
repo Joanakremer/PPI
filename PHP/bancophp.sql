8610fcfdfc5a2e95cfe7c6f30e259578baf1b7db
joana <joana.kt31@aluno.ifsc.edu.br>
2023-06-21 11:37:15 -0300

    banco php

---
+++ PHP/bancophp.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS ppidatabase;
+USE ppidatabase;
+
+CREATE TABLE `animal` (
+    `id` int  NOT NULL auto_increment,
+    `email` varchar(50)  NOT NULL ,
+    `senha` varchar(50)  NOT NULL ,
+    `nome` varchar(100)  NOT NULL ,
+    `raca` varchar(50)  NOT NULL ,
+    `idade` int  NOT NULL ,
+    `peso` float  NOT NULL ,
+    PRIMARY KEY (`id`)
+);
